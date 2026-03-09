4da5b7e0a5f48887bf9d45f50c1d7ef06e10f99a
arjayAbrenica <arjayabrenica30@gmail.com>
2026-03-09 11:41:43 +0800

    with updated html

--- bsp-inventory-management-system/frontend/src/app/app.ts
+++ bsp-inventory-management-system/frontend/src/app/app.ts
@@ -4,7 +4,7 @@ import { Login } from './features/login/login';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login],
+  imports: [RouterOutlet,Login],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
